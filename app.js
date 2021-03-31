54d53d6b2f7b2bf82884fc8580b2b7f7a1c1686b
Lesley Kimutai <leskimuti@gmail.com>
2021-03-31 09:37:48 +0300

    Second Commit - Database Encryption

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -13,10 +14,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/usersDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema ={
+const userSchema =new mongoose.Schema ({
   email: String,
   password: String
-};
+});
+
+
+// ///////////////////USING MONGOOSE ENCRYPT /////////////////
+const secret = "Thisisalongstringsecret";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+// const secret = process.env.SOME_LONG_UNGUESSABLE_STRING;
 
 const User = new mongoose.model("User", userSchema);
 
