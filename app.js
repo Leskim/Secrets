42acf58d7e00add3b72c4311e8fedef8277b36b2
Lesley Kimutai <leskimuti@gmail.com>
2021-03-31 13:00:39 +0300

    Fourth Commit - Hash Password

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -20,11 +20,6 @@ const userSchema =new mongoose.Schema ({
   password: String
 });
 
-
-// ///////////////////USING MONGOOSE ENCRYPT /////////////////
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-// const secret = process.env.SOME_LONG_UNGUESSABLE_STRING;
 
 const User = new mongoose.model("User", userSchema);
 
@@ -41,10 +36,10 @@ app.get("/register", function(req,res){
 });
 
 app.post("/register", function(req, res){
-
+//////// Hashing the user,s Password /////////////
    const newUser = new User ({
      email: req.body.username,
-     password: req.body.password
+     password: md5(req.body.password)
    });
    newUser.save(function(err){
      if(err){
@@ -57,8 +52,9 @@ app.post("/register", function(req, res){
 });
 
 app.post("/login", function(req, res){
+  // ///////////// Checking if hashed password is equivalent to the password the user provided //////
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if(err){
